2ecf39c877acb14d5cdf582f1a7bdc66452f5112
Gregorio Juliana <gregojquiros@gmail.com>
2016-09-11 17:16:49 +0200

    Fixes

--- MacroHardDoorsServer/routes/admins.js
+++ MacroHardDoorsServer/routes/admins.js
@@ -1,13 +1,13 @@
 var express = require('express'),
-    userController = require('../controllers/userController.js'),
+    adminController = require('../controllers/adminController.js'),
     multer = require('multer');
 
 var router = express.Router();
 
-router.route('/register').post([multer({ dest: './uploads/' })], userController.register);
-router.route('/unregister').post(userController.unRegister);
-router.route('/adminlogin').post(userController.doAdminLogin);
-router.route('/newadmin').post([multer({ dest: './uploads/' })], userController.createNewAdmin);
-router.route('/:admin').get(userController.getAdminInfo);
+router.route('/register').post([multer({ dest: './uploads/' })], adminController.register);
+router.route('/unregister').post(adminController.unRegister);
+router.route('/adminlogin').post(adminController.doAdminLogin);
+router.route('/newadmin').post([multer({ dest: './uploads/' })], adminController.createNewAdmin);
+router.route('/:admin').get(adminController.getAdminInfo);
 
 module.exports = router;
