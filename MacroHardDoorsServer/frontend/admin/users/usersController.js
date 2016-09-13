8d954886aeac17d322e92d4b3cb2dc3cb004dc9c
sirasistant <sirasistant@gmail.com>
2016-09-13 15:36:39 +0200

    Implemented new user validation

--- MacroHardDoorsServer/frontend/admin/users/usersController.js
+++ MacroHardDoorsServer/frontend/admin/users/usersController.js
@@ -4,8 +4,10 @@ DoorsAdmin.controller('usersController', ['$scope','$location','$http','$modal',
 		$http.get('/api/users').success(function(data,status){
 			$scope.users=data;
 		});
-	}
+	};
+
 	reloadUsers();
+
 	$scope.addUser=function(){
 		var modalInstance = $modal.open({
 			templateUrl: 'addUser.html',
@@ -19,42 +21,45 @@ DoorsAdmin.controller('usersController', ['$scope','$location','$http','$modal',
 		}, function () {
 			$log.info('Modal dismissed at: ' + new Date());
 		});
-	}
+	};
+
 	$scope.deleteUser=function(user){
 		$http.post('/api/users/'+user._id+'/delete',{}).success(function(data,status){
 			if(status==200){
 				reloadUsers();
 			}
 		});
-	}
+	};
+
 	$scope.setUserActive=function(user,value){
 		$http.post('/api/users/'+user._id+'/activate',{active:value}).success(function(data,status){
 			if(status==200){
 				reloadUsers();
 			}
 		});
-	}
+	};
 	$scope.viewUser=function(user){
 		$location.path('/users/'+user._id);
 	}
 }]);
 DoorsAdmin.controller('addUserCtrl',  ['$http', '$scope','$modalInstance', function ($http, $scope, $modalInstance) {
 	$scope.ok = function () {
-		var formData = new FormData();
-		formData.append("profilePic", $("#image")[0].files[0]);
-		formData.append("name", $scope.name);
-		formData.append("alias", $scope.alias);
-		formData.append("password", CryptoJS.SHA256($scope.password).toString().toUpperCase());
-		formData.append("email",$scope.mail);
-		$http.post('/api/users/newuser', formData, {
-			transformRequest: angular.identity,
-			headers: { 'Content-Type': undefined }
-		}).success(function (data, status) {
-			$modalInstance.close();
-		}).error(function(data, status){
-			alert(data);
-		});
-
+		if($scope.addUserForm.$valid&&$("#image")[0].files[0]) {
+			var formData = new FormData();
+			formData.append("profilePic", $("#image")[0].files[0]);
+			formData.append("name", $scope.name);
+			formData.append("alias", $scope.alias);
+			formData.append("password", CryptoJS.SHA256($scope.password).toString().toUpperCase());
+			formData.append("email", $scope.mail);
+			$http.post('/api/users/newuser', formData, {
+				transformRequest: angular.identity,
+				headers: {'Content-Type': undefined}
+			}).success(function (data, status) {
+				$modalInstance.close();
+			}).error(function (data, status) {
+				alert(data);
+			});
+		}
 	};
 
 	$scope.cancel = function () {
