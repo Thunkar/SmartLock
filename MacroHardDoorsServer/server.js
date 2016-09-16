61aa5412568cbfc647d679852b3c4463179ce4f2
Gregorio Juliana <gregojquiros@gmail.com>
2016-09-16 10:49:28 +0200

    Fixes

--- MacroHardDoorsServer/server.js
+++ MacroHardDoorsServer/server.js
@@ -56,10 +56,10 @@ services.init().then(() => {
 
     if (config.logLevel == "debug") {
         app.use((req, res, next) => {
-            var user = { username: "unknown" };
+            var user = { alias: "unknown" };
             if (req.session && req.session.user)
                 var user = req.session.user;
-            var logLine = "[" + user.username + "] " + "[" + req.originalUrl + "] ";
+            var logLine = "[" + user.alias + "] " + "[" + req.originalUrl + "] ";
             systemLogger.debug(logLine);
             next();
         });
