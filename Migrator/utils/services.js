0eafec7e1a9003401b1c3fd78bdca6a0cc5a8a88
Unknown <gregojquiros@gmail.com>
2016-10-06 12:59:34 +0200

    Fix

--- Migrator/utils/services.js
+++ Migrator/utils/services.js
@@ -3,27 +3,35 @@
 
 var services = {};
 
-var init = function () {
-    try {
-        var config = JSON.parse(fs.readFileSync('./config.cnf', 'utf8').toString());
-    } catch (err) {
-        console.error("No config");
-        process.exit(-1);
-    }
-    services.config = config;
-    require('./services/logger.js');
-    var servicesLogger = winston.loggers.get('services');
-    servicesLogger.info("Loading services");
-    var files = fs.readdirSync("./utils/services").filter(function (file) {
-        return file.indexOf(".js") != -1 && file != "logger.js";
-    }).sort(function(a, b){
-       return b == "dbLoader.js" ? 1 : -1; 
+function init() {
+    return new Promise((resolve, reject) => {
+        try {
+            var config = JSON.parse(fs.readFileSync('./config.cnf', 'utf8').toString());
+        } catch (err) {
+            console.error("No config");
+            process.exit(-1);
+        }
+        services.config = config;
+        require('./services/logger.js');
+        servicesLogger = winston.loggers.get('services');
+        servicesLogger.info("Loading services");
+        var files = fs.readdirSync("./utils/services").filter((file) => {
+            return file.indexOf(".js") != -1 && file != "logger.js";
+        }).sort((a, b) => {
+            return b == "dbLoader.js" ? 1 : -1;
+        });
+        async.eachSeries(files, (file, callback) => {
+            var service = require("./services/" + file);
+            services[file.replace('.js', '')] = service;
+            service.init().then(callback);
+        }, (err) => {
+            if(err) return reject(err);
+            servicesLogger.info("Service loading completed");
+            return resolve();
+        });
+    }, (err) => {
+        return reject(err);
     });
-
-    files.forEach(function (file) {
-        services[file.replace('.js', '')] = require("./services/" + file);
-    });
-    servicesLogger.info("Service loading completed");
 }
 
 services.init = init;
